cba539b1df561c8dceae42191c0b6a1745d3aff1
pierredm1 <66529459+pierredm1@users.noreply.github.com>
2022-08-29 19:26:41 +0200

    cleaner code and almost working

--- pierre_algo.py
+++ pierre_algo.py
@@ -1,34 +1,54 @@
 import pandas as pd
 import numpy as np
 
+#fix table
+
 def gradients_fct(data, window):
 
     SMA = data.rolling(window=window).mean()
-    gradients_prov = pd.DataFrame.pct_change(SMA)
 
+    gradients_prov = pd.DataFrame.pct_change(SMA)
     gradients = pd.Series(gradients_prov).fillna(0)
     second_gradients = pd.Series(np.gradient(gradients))
 
     return gradients, second_gradients, SMA
 
-def buy(sigPriceBuy, sigPriceSell, flag=1):
+def buy(profit, price_bought_at,price_short_at, i, data, sigPriceBuy, sigPriceSell, flag=1):
+    if flag<0:
+        profit += data[i] - sum(price_short_at)/len(price_short_at)
+
     flag += 1
     sigPriceBuy.append('BUY')
     sigPriceSell.append(0)
-    return sigPriceBuy, sigPriceSell, flag
 
+    price_bought_at = update_long_price(i,price_bought_at, data)
 
-def sell(sigPriceBuy, sigPriceSell, flag=-1):
+    return profit, price_bought_at, price_short_at, i, data, sigPriceBuy, sigPriceSell, flag
+
+def sell(profit, i,price_bought_at, price_short_at, data, sigPriceBuy, sigPriceSell, flag=-1):
+    if flag>0:
+        profit += sum(price_bought_at)/len(price_bought_at) - data[i]
+
     flag += -1
+
     sigPriceBuy.append(0)
     sigPriceSell.append('SELL')
-    return sigPriceBuy, sigPriceSell, flag
 
-def update_short_price():
-    pass
+    price_short_at = update_short_price(i,price_short_at, data)
 
-def update_long_price():
-    pass
+    return profit, i, price_bought_at, price_short_at, data, sigPriceBuy, sigPriceSell, flag
+
+def update_short_price(i,price_short_at, data):
+    price_short_at.append(data[i])
+    if len(price_short_at)>5:
+        price_short_at = price_short_at[1:]
+    return price_short_at
+
+def update_long_price(i,price_bought_at, data):
+    price_bought_at.append(data[i])
+    if len(price_bought_at)>5:
+        price_bought_at = price_bought_at[1:]
+    return price_bought_at
 
 def inflexion_primary(data, window):
 
@@ -38,9 +58,10 @@ def inflexion_primary(data, window):
     sigPriceSell = []
     flag = 0
     flag_status = []
-    daily_profit = []
+    daily_profit = [0]
     profit = 0
-    trade_base = 100000  #100,000
+    price_short_at=[]
+    price_bought_at=[]
 
     ###########BASE PROFIT OVERALL
     for i in range(len(data)):
@@ -48,126 +69,132 @@ def inflexion_primary(data, window):
         flag_status.append(flag)
 
         # TAKING A LONG POSITION
-        if gradients[i] > 0 and gradients[i - 1] <= 0:
+        if gradients[i] > 0 and gradients[i-1] <= 0:
 
             #From nothing to long
             if flag == 0:
-                price_bought_at = data[i]
-                buy(sigPriceBuy, sigPriceSell, flag)
+                profit, price_bought_at, price_short_at, i, data, sigPriceBuy, sigPriceSell, flag = buy(
+                    profit, price_bought_at, price_short_at,i, data, sigPriceBuy,
+                    sigPriceSell, flag)
 
             #From short to a long
             elif flag <= -1 :
-                profit += price_short_at - data[i]
-                price_bought_at = data[i]
-                buy(sigPriceBuy, sigPriceSell, flag)
+                profit, price_bought_at, price_short_at, i, data, sigPriceBuy, sigPriceSell, flag = buy(
+                    profit, price_bought_at, price_short_at, i, data, sigPriceBuy,
+                    sigPriceSell, flag)
 
         # TAKING A SHORT POSITION
         elif gradients[i] < 0 and gradients[i - 1] >= 0:
-
             #from nothing to a short
             if flag == 0:
-                price_short_at = data[i]
-                sell(sigPriceBuy, sigPriceSell, flag)
+                profit, i, price_bought_at, price_short_at, data, sigPriceBuy, sigPriceSell, flag = sell(
+                    profit, i, price_bought_at, price_short_at, data, sigPriceBuy,
+                    sigPriceSell, flag)
 
             #from long to a short
             elif flag >= 1:
-                profit += data[i] - price_bought_at
-                price_short_at = data[i]
-                sell(sigPriceBuy, sigPriceSell, flag)
+                profit, i, price_bought_at, price_short_at, data, sigPriceBuy, sigPriceSell, flag = sell(
+                    profit, i, price_bought_at, price_short_at, data, sigPriceBuy,
+                    sigPriceSell, flag)
 
             #from short to more short
         elif flag <= -1 or flag >= 1:
-            inflexion_secondary(i, data, gradients, second_gradients,
-                                    sigPriceBuy, SMA, sigPriceSell, profit,
-                                    flag_status, daily_profit)
-            pass
+            sigPriceBuy, sigPriceSell, profit, flag_status, daily_profit, price_bought_at, price_short_at, flag = inflexion_secondary(
+                i, data, gradients, second_gradients, sigPriceBuy, SMA,
+                sigPriceSell, profit, flag_status, daily_profit,
+                price_short_at, price_bought_at, flag)
 
         else:
+
             sigPriceBuy.append(0)
             sigPriceSell.append(0)
 
-    return gradients, second_gradients, sigPriceBuy,SMA, sigPriceSell, profit, flag_status, daily_profit
+    return gradients, second_gradients,SMA, sigPriceBuy, sigPriceSell, profit, flag_status, daily_profit
 
-def inflexion_secondary(i, data, gradients, second_gradients, sigPriceBuy, SMA, sigPriceSell, profit, flag_status, daily_profit):
+def inflexion_secondary(i, data, gradients, second_gradients, sigPriceBuy,
+                        SMA, sigPriceSell, profit, flag_status, daily_profit,
+                        price_short_at, price_bought_at, flag):
+
     #buy more long or begin to sell short
     if ((second_gradients[i] > 0 and gradients[i - 1] > 0 and gradients[i] > 0)
         or (second_gradients[i] > 0 and gradients[i - 1] < 0 and gradients[i] < 0)):
 
-        price_bought_at = data[i]
-        buy(sigPriceBuy, sigPriceSell)
-        sigPriceBuy.append('BUY')
-        sigPriceSell.append(0)
-        return sigPriceBuy, sigPriceSell, profit, flag_status, daily_profit
+        profit, price_bought_at, price_short_at, i, data, sigPriceBuy, sigPriceSell, flag = buy(
+            profit, price_bought_at, price_short_at, i, data, sigPriceBuy,
+            sigPriceSell, flag)
+
+        return sigPriceBuy, sigPriceSell, profit, flag_status, daily_profit, price_bought_at, price_short_at, flag
 
     #sell more short or begin to sell long
     elif ((second_gradients[i] < 0 and gradients[i - 1] < 0 and gradients[i] < 0) or
         (second_gradients[i] < 0 and gradients[i - 1] > 0 and gradients[i] > 0)):
 
-        price_short_at = data[i]
-        sell(sigPriceBuy, sigPriceSell)
-        sigPriceBuy.append(0)
-        sigPriceSell.append('SELL')
-        return sigPriceBuy, sigPriceSell, profit, flag_status, daily_profit
+        profit, i, price_bought_at, price_short_at, data, sigPriceBuy, sigPriceSell, flag = sell(
+            profit, i, price_bought_at, price_short_at, data, sigPriceBuy,
+            sigPriceSell, flag)
+
+        return sigPriceBuy, sigPriceSell, profit, flag_status, daily_profit, price_bought_at, price_short_at, flag
 
     else:
         sigPriceBuy.append(0)
         sigPriceSell.append(0)
-        return sigPriceBuy, sigPriceSell, profit, flag_status, daily_profit
+        return sigPriceBuy, sigPriceSell, profit, flag_status, daily_profit, price_bought_at, price_short_at, flag
 
 
 def position_array(data, flag_status, daily_profit):
 
-    position = []
+    position = [0]
     trade_base = 100000 #100,000
 
-    current_position = 0
-    for i in range(0, len(data)):
+    for i in range(1,len(data)):
+        diff = data[i - 1] - data[i]
+        daily_profit.append(diff*flag_status[i])
+        position.append(trade_base*flag_status[i])
 
-        if flag_status[i] == 0:
-            daily_profit.append(0)
-            position.append(0)
-
-        if flag_status[i] == 1:
-            daily_profit.append(data[i] - data[i - 1])
-            position.append(trade_base)
-
-        if flag_status[i] == -1:
-            daily_profit.append(data[i - 1] - data[i])
-            position.append(-trade_base)
-
-    return position
+    return daily_profit, position
 
 
 
 def table_generation(data, window):
 
     gradients, second_gradients, SMA, sigPriceBuy, sigPriceSell, profit, flag_status, daily_profit = inflexion_primary(data,window)
-    position = position_array(data, flag_status, daily_profit)
+    daily_profit, position = position_array(data, flag_status, daily_profit)
 
     cumulative_profit = np.cumsum(daily_profit)
 
-    table = pd.DataFrame({
-        'PRICE': data,
-        'GRADIENTS': gradients,
-        'SECOND_GRADIENTS': second_gradients,
-        'SMA': SMA,
-        'BUY_SIGNALS': sigPriceBuy,
-        'SELL_SIGNALS': sigPriceSell,
-        'DIRECTIONAL EXPOSURE': flag_status,
-        'POSITION':position,
-        'DAILY PROFIT': daily_profit,
-        'CUMULATIVE PROFIT': cumulative_profit
-    })
-    return table, profit
+    print(len(data))
+    print(len(gradients))
+    print(len(second_gradients))
+    print(len(SMA))
+    print(len(sigPriceBuy))
+    print(len(sigPriceSell))
+    print(len(flag_status))
+    print(len(position))
+    print(len(daily_profit))
+    print(len(cumulative_profit))
+
+    #table = pd.DataFrame({
+    #'PRICE': data, 2607
+    #'GRADIENTS': gradients, 2607
+    #'SECOND_GRADIENTS': second_gradients, 2607
+    #'SMA': SMA, 2571
+    #'BUY_SIGNALS': sigPriceBuy, 2607
+    #'SELL_SIGNALS': sigPriceSell, 2571
+    #'DIRECTIONAL EXPOSURE': flag_status,  2607
+    #'POSITION':position, 2607
+    #'DAILY PROFIT': daily_profit, 2607
+    #'CUMULATIVE PROFIT': cumulative_profit 2607
+    #})
+    return profit #, table
 
 
 
 def algo_call(series, window):
     data = pd.read_excel('Data/Time Series Data.xlsx', index_col='Day')
     df = data['Series {}'.format(series)]
-    table, profit = table_generation(df, window)
-    print(profit)
-    return table, profit
+    profit = table_generation(df, window)
+    print('the profit is'+str(profit))
+    return profit #,table
 
 if __name__ == '__main__':
-    algo_call(13,15)
+    algo_call(4,15)
